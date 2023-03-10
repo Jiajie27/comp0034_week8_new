32b77ed95b286131da1457b15b044f5aab5bdc3c
Jiajie27 <zcemhuc@ucl.ac.uk>
2023-03-10 18:01:59 +0000

    models.py

--- paralympic_app/__init__.py
+++ paralympic_app/__init__.py
@@ -1,20 +1,44 @@
+from pathlib import Path
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
-from . import api_routes
+
+# Sets the project root folder
+PROJECT_ROOT = Path(__file__).parent
+
+# Create a global SQLAlchemy object
+db = SQLAlchemy()
+# Create a global Flask-Marshmallow object
+ma = Marshmallow()
 
 
 def create_app():
     """Create and configure the Flask app"""
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "YY3R4fQ5OmlmVKOSlsVHew"
+    app.config["SECRET_KEY"] = 'zZzb_3n4Ko70RglF6MTFvg'
+    # configure the SQLite database location
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + str(
+        PROJECT_ROOT.joinpath("data", "paralympics.db")
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = True
 
-    # Register the api blueprint for the routes in api_routes.py
-    from .api_routes import bp
+    # Uses a helper function to initialise extensions
+    initialize_extensions(app)
 
-    app.register_blueprint(bp)
-
-    # Register the routes in paralypic_routes.py
+    # Include the routes from routes.py
     with app.app_context():
         from . import paralympic_routes
+    # This is required as you must instantiate the models before marshamallow schemas
+        from paralympic_app.models import Event, Region
 
     return app
+
+
+def initialize_extensions(app):
+    """Binds extensions to the Flask application instance (app)"""
+    # Flask-SQLAlchemy
+    db.init_app(app)
+    # Flask-Marshmallow
+    ma.init_app(app) 
--- paralympic_app/paralympic_routes.py
+++ paralympic_app/paralympic_routes.py
@@ -1,7 +1,35 @@
+import sys
 from flask import render_template, current_app as app
+from paralympic_app import db
+from paralympic_app.models import Region, Event
+from .schemas import RegionSchema, EventSchema
 
+regions_schema = RegionSchema(many=True)
+regions_schema = RegionSchema()
+events_schema = EventSchema(many=True)
+events_schema = EventSchema()
 
 @app.route("/")
 def index():
     """Creates a homepage for the paralympic app"""
     return render_template("index.html")
+
+@app.get("/noc")
+def noc():
+    """Returns a list of NOC region codes and their details in JSON."""
+    # Select all the regions using Flask-SQLAlchemy
+    all_regions = db.session.execute(db.select(Region)).scalars()
+    print(all_regions, file=sys.stderr)
+    # Get the data using Marshmallow schema (returns JSON)
+    result = regions_schema.dump(all_regions)
+    # Return the data
+    return result
+
+
+@app.get("/event/<int:event_id>")
+def event_id(event_id):
+    """Returns the details for a specified event id"""
+    event = db.session.execute(
+        db.select(Event).filter_by(event_id=event_id)
+    ).scalar_one_or_none()
+    return events_schema.dump(event)
